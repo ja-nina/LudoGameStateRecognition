ee37f6b53e75b372cdeb0400e5d9c3fb99d75ae6
ja-nina <zukowskanina1@gmail.com>
2022-12-26 21:54:23 +0100

    dynamic recognition creation

---
+++ dynamic_recognition.py
@@ -0,0 +1,93 @@
+# goal of this file is to basically see where tokens are
+import cv2
+import numpy as np
+from config import *
+def four_point_transform(image, pts):
+    '''
+    Transforms photo of board in a weird angle into nice board that can be compared to field placements of original
+    '''
+    rect = order_points(pts)
+    print("rect: ",  rect)
+    (tl, tr, br, bl) = rect
+
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype = "float32")
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    # return the warped image
+    return warped
+
+
+def order_points(pts):
+        print(pts)
+        rect = np.zeros((4, 2), dtype = "float32")
+        s = pts.sum(axis = 1)
+        print("lol", s, np.argmin(s), pts[1][:2])
+        rect[0] = pts[np.argmin(s)][:2] + [WIDTH_BOXIE, WIDTH_BOXIE]
+        rect[2] = pts[np.argmax(s)][:2] + [WIDTH_BOXIE, WIDTH_BOXIE]
+        diff = np.diff(pts, axis = 1)
+        print(diff[:,0], np.argmin(diff))
+        rect[1] = pts[np.argmin(diff[:,0])][:2] + [WIDTH_BOXIE, WIDTH_BOXIE]
+        rect[3] = pts[np.argmax(diff[:,0])][:2] + [WIDTH_BOXIE, WIDTH_BOXIE]
+        return rect
+
+def get_tokens(frame, title = " default gray blob token detection on difference"):
+        frame_copy = frame.copy()
+        frame_blurred = cv2.medianBlur(frame_copy, 3)
+        frame_gray = cv2.cvtColor(frame_blurred, cv2.COLOR_BGR2GRAY)
+        mask = cv2.inRange(frame_gray, 60, 255)
+        #mask = cv2.inRange(frame_hsv, lower_green_tokens, upper_green_tokens)
+        #mask = cv2.inRange(frame_hsv, lower_red_tokens, upper_red_tokens)
+        frame_gray[mask == 0] = 0
+        # Creating maxican hat filter
+        #filter = np.array([[0,0,-1,0,0],[0,-1,-2,-1,0],[-1,-2,16,-2,-1],[0,-1,-2,-1,0],[0,0,-1,0,0]])
+        # Applying cv2.filter2D function on our Logo image
+        #mexican_hat_img2=cv2.filter2D(frame_gray,-1,filter)
+        t_lower = 20 
+        t_upper = 250
+        #canny = cv2.Canny(frame_gray, t_lower, t_upper)
+        kernel = np.ones((3, 3), np.uint8)
+        
+        # erode the image
+        #erosion = cv2.erode(frame_gray, kernel, iterations=1)
+        erosion = frame_gray
+        blobs = detector.detect(erosion)
+
+        # overlaying info 
+        for blob in blobs:
+                pos = blob.pt
+                r = blob.size / 2
+                cv2.circle(erosion, (int(pos[0]), int(pos[1])), int(r), (255, 0, 0), 2)
+                cv2.putText(erosion, str(blob.response),
+                    (int(pos[0]), int(pos[1])),
+                    cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
+        resized_eroded = cv2.resize(erosion,(500,500), interpolation = cv2.INTER_AREA)
+
+        #cv2.imshow("token_detection", resized_eroded)
+        
+        return frame_copy
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -2,7 +2,8 @@ import cv2
 import numpy as np
 import random
 import matplotlib.pyplot as plt 
-import itertools
+from static_recognition import find_squares
+from dynamic_recognition import four_point_transform
 from config import *
 from random import randint
 trackerTypes = ['BOOSTING']#, 'MIL', 'KCF','TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
@@ -122,9 +123,11 @@ def calculate_bases(image):
 
 def add_tuples(t1,t2):
         return tuple([e1+e2 for e1,e2 in zip(t1,t2)])
+
 def get_corner_players(board):
         '''
         done on aleady resized board 500 x 500
+        determines if 'original' board is rotated
         '''
         print("Shape of board: ", board.shape)
         width_board, heigh_board, board_depth  = board.shape
@@ -179,97 +182,9 @@ def get_corner_players(board):
 def get_who_plays():
         pass
 
-def get_tokens(frame, title = " default gray blob token detection on difference"):
-        frame_copy = frame.copy()
-        frame_blurred = cv2.medianBlur(frame_copy, 3)
-        frame_gray = cv2.cvtColor(frame_blurred, cv2.COLOR_BGR2GRAY)
-        mask = cv2.inRange(frame_gray, 60, 255)
-        #mask = cv2.inRange(frame_hsv, lower_green_tokens, upper_green_tokens)
-        #mask = cv2.inRange(frame_hsv, lower_red_tokens, upper_red_tokens)
-        frame_gray[mask == 0] = 0
-        # Creating maxican hat filter
-        #filter = np.array([[0,0,-1,0,0],[0,-1,-2,-1,0],[-1,-2,16,-2,-1],[0,-1,-2,-1,0],[0,0,-1,0,0]])
-        # Applying cv2.filter2D function on our Logo image
-        #mexican_hat_img2=cv2.filter2D(frame_gray,-1,filter)
-        t_lower = 20 
-        t_upper = 250
-        #canny = cv2.Canny(frame_gray, t_lower, t_upper)
-        kernel = np.ones((3, 3), np.uint8)
-        
-        # erode the image
-        #erosion = cv2.erode(frame_gray, kernel, iterations=1)
-        erosion = frame_gray
-        blobs = detector.detect(erosion)
-
-        # overlaying info 
-        for blob in blobs:
-                pos = blob.pt
-                r = blob.size / 2
-                cv2.circle(erosion, (int(pos[0]), int(pos[1])), int(r), (255, 0, 0), 2)
-                cv2.putText(erosion, str(blob.response),
-                    (int(pos[0]), int(pos[1])),
-                    cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
-        resized_eroded = cv2.resize(erosion,(500,500), interpolation = cv2.INTER_AREA)
-
-        #cv2.imshow("token_detection", resized_eroded)
-        
-        return frame_copy
-
-
 def calculate_circles(image):
 
         pass
-
-def order_points(pts):
-        print(pts)
-        rect = np.zeros((4, 2), dtype = "float32")
-        s = pts.sum(axis = 1)
-        print("lol", s, np.argmin(s), pts[1][:2])
-        rect[0] = pts[np.argmin(s)][:2] + [WIDTH_BOXIE, WIDTH_BOXIE]
-        rect[2] = pts[np.argmax(s)][:2] + [WIDTH_BOXIE, WIDTH_BOXIE]
-        diff = np.diff(pts, axis = 1)
-        print(diff[:,0], np.argmin(diff))
-        rect[1] = pts[np.argmin(diff[:,0])][:2] + [WIDTH_BOXIE, WIDTH_BOXIE]
-        rect[3] = pts[np.argmax(diff[:,0])][:2] + [WIDTH_BOXIE, WIDTH_BOXIE]
-        return rect
-
-def four_point_transform(image, pts):
-    rect = order_points(pts)
-    print("rect: ",  rect)
-    (tl, tr, br, bl) = rect
-
-    # compute the width of the new image, which will be the
-    # maximum distance between bottom-right and bottom-left
-    # x-coordiates or the top-right and top-left x-coordinates
-    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-    maxWidth = max(int(widthA), int(widthB))
-
-    # compute the height of the new image, which will be the
-    # maximum distance between the top-right and bottom-right
-    # y-coordinates or the top-left and bottom-left y-coordinates
-    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-    maxHeight = max(int(heightA), int(heightB))
-
-    # now that we have the dimensions of the new image, construct
-    # the set of destination points to obtain a "birds eye view",
-    # (i.e. top-down view) of the image, again specifying points
-    # in the top-left, top-right, bottom-right, and bottom-left
-    # order
-    dst = np.array([
-        [0, 0],
-        [maxWidth - 1, 0],
-        [maxWidth - 1, maxHeight - 1],
-        [0, maxHeight - 1]], dtype = "float32")
-
-    # compute the perspective transform matrix and then apply it
-    M = cv2.getPerspectiveTransform(rect, dst)
-    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-
-    # return the warped image
-    return warped
-
         
 if __name__ == "__main__":
     colors = []
@@ -295,20 +210,18 @@ if __name__ == "__main__":
     res = cv2.waitKey(1)
     
     prev = cv2.imread('version2.png')
-    get_grid(prev)
     dim = (500,500)
     prev = cv2.resize(prev, dim, interpolation = cv2.INTER_AREA)
-    #get_corner_players(prev)
-    #bbox = (88, 68, 886, 920)
-    #multiTracker = cv2.MultiTracker_create()
+    get_corner_players(prev)
+    bbox = (88, 68, 886, 920)
+    multiTracker = cv2.MultiTracker_create()
     # Initialize MultiTracker
-    #for bbox in bboxes.values():
-    #    for corner in bbox:
-    #            print(" corner: ", (corner[0], corner[1] , corner[0] + 20, corner[1] + 20))
-    #            for trackerType in trackerTypes:
-    #                    multiTracker.add(createTrackerByName(trackerType), frame, (corner[0] - WIDTH_BOXIE , corner[1] -WIDTH_BOXIE, 40, 40))
+    for bbox in bboxes.values():
+        for corner in bbox:
+                print(" corner: ", (corner[0], corner[1] , corner[0] + 20, corner[1] + 20))
+                for trackerType in trackerTypes:
+                        multiTracker.add(createTrackerByName(trackerType), frame, (corner[0] - WIDTH_BOXIE , corner[1] -WIDTH_BOXIE, 40, 40))
     while(cap.isOpened()):
-
 
         ret, frame = cap.read()
         
@@ -341,7 +254,6 @@ if __name__ == "__main__":
         
         print("transform")
         frame = four_point_transform(frame, boxes)
-        frame_w_blobs, _ = get_blobs(frame)
 
 
         scale_percent = 50 # percent of original size
@@ -368,7 +280,7 @@ if __name__ == "__main__":
         
                 #resized2 = cv2.resize(difference, dim, interpolation = cv2.INTER_AREA)
                 blobous_difference = get_tokens(difference)
-                #cv2.imshow("difference", blobous_difference)
+                cv2.imshow("difference", blobous_difference)
 
         res = cv2.waitKey(1)
 
